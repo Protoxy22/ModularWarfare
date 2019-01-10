4cd2d44df61477603264221007f6b5be4eb90338
TheWillyJ <41086203+TheWillyJ@users.noreply.github.com>
2019-01-09 22:09:37 -0400

    RenderArms update

--- src/main/java/com/modularwarfare/client/model/RenderArms.java
+++ src/main/java/com/modularwarfare/client/model/RenderArms.java
@@ -5,6 +5,15 @@ import org.lwjgl.util.vector.Vector3f;
 
 public class RenderArms {
 	
+	/*
+	 * GL Calls are run from the bottom up
+	 * For ease of use the following order should be followed at all times;
+	 * glScale
+	 * glTranslate
+	 * glRotate
+	 * This will allow for translations without having to account for rotation
+	 */
+	
 	// right hand pump action animation
 	public static void renderArmPump(ModelGun model, GunAnimations anim, float smoothing, Vector3f rotationPoint, Vector3f armPosition)
 	{
@@ -18,7 +27,6 @@ public class RenderArms {
 	// hand reload with right hand charge)
 	public static void renderArmCharge(ModelGun model, GunAnimations anim, float smoothing, Vector3f rotationPoint, Vector3f armPosition)
 	{
-		handleRotate(rotationPoint);
 		GL11.glTranslatef(
 				-(armPosition.x
 						- Math.abs(anim.lastCharged + (anim.charged - anim.lastCharged) * smoothing)
@@ -29,13 +37,13 @@ public class RenderArms {
 				(-(armPosition.z
 						- Math.abs(anim.lastCharged + (anim.charged - anim.lastCharged) * smoothing)
 								/ model.chargeModifier.z)));
+		handleRotate(rotationPoint);
 	}
 	
 	// This moves the right hand if leftHandAmmo & handBolt are true (For left hand
 	// reload with right hand bolt action)
 	public static void renderArmBolt(ModelGun model, GunAnimations anim, float smoothing, Vector3f rotationPoint, Vector3f armPosition)
 	{
-		handleRotate(rotationPoint);
 		GL11.glTranslatef(
 				(armPosition.x + Math.abs(anim.lastPumped + (anim.pumped - anim.lastPumped) * smoothing)
 						/ model.chargeModifier.x),
@@ -45,18 +53,19 @@ public class RenderArms {
 				(-(armPosition.z
 						- Math.abs(anim.lastCharged + (anim.charged - anim.lastCharged) * smoothing)
 								/ model.chargeModifier.z)));
+		handleRotate(rotationPoint);
 	}
 	
 	public static void renderArmDefault(ModelGun model, GunAnimations anim, float smoothing, Vector3f rotationPoint, Vector3f armPosition)
 	{
-		handleRotate(rotationPoint);
 		GL11.glTranslatef(armPosition.x, armPosition.y, armPosition.z);
+		handleRotate(rotationPoint);
 	}
 	
 	public static void renderArmReload(ModelGun model, GunAnimations anim, float smoothing, Vector3f rotationPoint, Vector3f armPosition)
 	{
-		handleRotate(rotationPoint);
 		GL11.glTranslatef(armPosition.x, armPosition.y, armPosition.z);
+		handleRotate(rotationPoint);
 	}
 	
 	private static void handleRotate(Vector3f rotationPoint)
